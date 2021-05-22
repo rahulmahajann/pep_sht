215a82dc0388f3b46228a7519604030b7bf85c6b
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-05-22 22:43:16 +0530

    added

--- array&strng7.py
+++ array&strng7.py
@@ -5,3 +5,21 @@ class Solution:
         for _ in num:
             if(nums.count(_)>ans):
                 return _
+
+
+# APPROACH 2
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        m=nums[0]
+        count=1
+        for _ in range(1,len(nums)):
+            if(m==nums[_]):
+                count+=1
+            else:
+                count-=1
+                if(count==0):
+                    count=1
+                    m=nums[_]
+        return m
--- array&strng8.py
+++ array&strng8.py
@@ -9,4 +9,33 @@ class Solution:
 
 
 
-        # cr
+# Approach 2
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        elem1=None
+        elem2=None
+        count1=0
+        count2=0
+        for _ in nums:
+            if(elem1!=None and elem1==_):
+                count1+=1
+            elif(elem2!=None and elem2==_):
+                count2+=1
+            elif(count1==0):
+                elem1=_
+                count1+=1
+            elif(count2==0):
+                elem2=_
+                count2+=1
+            else:
+                count1-=1
+                count2-=1
+        ans=[]
+        if(nums.count(elem1)>(len(nums)//3)):
+            ans.append(elem1)
+        if(nums.count(elem2)>(len(nums)//3)):
+            ans.append(elem2)
+        
+        return ans
