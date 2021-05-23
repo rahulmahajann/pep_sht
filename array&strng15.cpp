88b1aa4806c2c6bda05bba0d42423b725d4d84de
Rahul Mahajan <rahulmahajan98645@gmail.com>
2021-05-24 02:00:22 +0530

    added

---
+++ array&strng15.cpp
@@ -0,0 +1,20 @@
+    // int sum=0;
+    // int maxi=0;
+    // int count=0;
+    // for(int i=0;i<n.size();i++){
+    //     if(n[i]<0){
+    //         count+=1;
+    //     }
+    //     sum+=n[i];
+    //     if(sum<0){
+    //         sum=0;
+    //     }
+    //     maxi=max(sum,maxi);
+        
+    // }
+    // sort(n.begin(),n.end());
+    // if(count==n.size()){
+    //     cout << (n[n.size()-1]);
+    // }else{
+    //     cout <<  maxi;
+    // }
